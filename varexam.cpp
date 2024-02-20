068e497ce692c4c551d3670e9fbcf63dca6362ff
Alvin Ho <alvinho@yandex.com>
2024-02-20 19:03:57 +0800

    exercise 2

--- varexam.cpp
+++ varexam.cpp
@@ -9,7 +9,7 @@ int main()
 
      Age = 35;
 
-     cout << "There was a nan named " << Name << endl; 
+     cout << "There was a man named " << Name << endl; 
      cout << "He was " << Age << "years old." << endl;
      return 0;
 
