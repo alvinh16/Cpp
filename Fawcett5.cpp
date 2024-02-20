6bd644caaea6801e1b91a83a96b647fe99e46831
Alvin Ho <alvinho@yandex.com>
2024-02-20 18:55:06 +0800

    initial exercise

---
+++ Fawcett5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+     cout << "Hello world!\n";
+     cout << "my name is fawcettr.\n\n";
+     cout << "please, call me farah!\n\n";
+     cout << "Hello world";
+     cout << "!" << endl;
+     cout << "Educative Team.\n";
+     return 0;
+
+}
