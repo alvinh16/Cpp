a3739945412f41f2d67f8c41899e7e25a10576e0
Alvin Ho <alvinho@yandex.com>
2024-03-02 19:50:31 +0800

    cleaning up

---
+++ Fawcett6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+     cout << "Hello world!\n";
+     cout << "Let's learn c++ !\n\n";
+     cout << "Hello world";
+     cout << "!" << endl;
+     cout << "this is a test.\n";
+     return 0;
+
+}
